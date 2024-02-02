b0d9488a595a59c3fbdd7fac51253d86347c2ce3
marcinlewinski <marcinlewinski71@gmail.com>
2024-02-02 20:27:41 +0100

    BACKEND_Refactor: The userDTO object, it is now being sent with roles

--- server/src/main/java/com/example/server/mapper/DtoMapperImpl.java
+++ server/src/main/java/com/example/server/mapper/DtoMapperImpl.java
@@ -6,8 +6,10 @@ import com.example.server.model.order.OrderItem;
 import com.example.server.model.order.dto.OrderItemResponseDto;
 import com.example.server.model.order.dto.OrderResponseDto;
 import com.example.server.model.order.dto.UserResponseDto;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -45,7 +47,7 @@ public class DtoMapperImpl implements DtoMapper {
     }
 
     @Override
-    public List<UserResponseDto>mapUserResponseDtoList(List<User> users) {
+    public List<UserResponseDto> mapUserResponseDtoList(List<User> users) {
         if (users == null) {
             return Collections.emptyList();
         }
@@ -57,6 +59,14 @@ public class DtoMapperImpl implements DtoMapper {
     }
 
     private UserResponseDto mapToUserResponseDto(User user) {
-        return new UserResponseDto(user.getEmail());
+        return new UserResponseDto(user.getEmail(), user.getName(), getRoles(user.getAuthorities()));
     }
+
+    private String getRoles(Collection<? extends GrantedAuthority> authorities) {
+        return authorities.stream().map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(" "));
+
+
+    }
+
 }
